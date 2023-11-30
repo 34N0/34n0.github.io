008abc594e0d4836b2d1b795c44a65505ded93b0
34n0 <34n0@immerda.ch>
2023-11-30 23:15:23 +0100

    feat: :sparkles: add public key

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,8 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/assets/js')
 	eleventyConfig.addPassthroughCopy('src/assets/fonts')
 	eleventyConfig.addPassthroughCopy('src/assets/images')
+	eleventyConfig.addPassthroughCopy('src/assets/keys')
+	eleventyConfig.addPassthroughCopy('public')
 
 
   /* --- PLUGINS --- */
